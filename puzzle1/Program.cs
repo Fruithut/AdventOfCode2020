c9df0cf76959f2f18d022ca87d1fe43dd2911277
Olav Gjerde <fruithut@tutanota.com>
2020-12-02 18:37:17 +0100

    Adds day 1 part 1

---
+++ puzzle1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+const int total = 2020;
+
+var allLines = File.ReadAllLines("./input.txt").Select(int.Parse).ToList();
+var numberDictionary = allLines.ToDictionary(value => value, value => true);
+
+var resultingMultiplication = 0;
+foreach (var inputValue in allLines)
+{
+    var differenceMissing = total - inputValue;
+    
+    if (numberDictionary.ContainsKey(differenceMissing))
+    {
+        resultingMultiplication = inputValue * differenceMissing;
+        break;
+    }
+}
+
+Console.WriteLine($"Result part one: {resultingMultiplication}");
