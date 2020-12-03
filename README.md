4838ead8493e2f82978561efc253c1bca506e0d4
Olav Gjerde <fruithut@tutanota.com>
2020-12-03 14:46:39 +0100

    Adds day 3 part 1 & 2

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Solutions to [Advent Of Code 2020](https://adventofcode.com/2020)
 |-----|------|----------|
 |[1: Report Repair](https://adventofcode.com/2020/day/1)|[1+2](/puzzle1/Program.cs)|C#
 |[2: Password Philosophy](https://adventofcode.com/2020/day/2)|[1+2](/puzzle2/Program.cs)|C#
+|[3: Toboggan Trajectory](https://adventofcode.com/2020/day/3)|[1+2](/puzzle3/Program.cs)|C#
---
+++ puzzle3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+var inputRows = File.ReadAllLines("./input.txt");
+var rowLength = inputRows[0].Length;
+
+var treeCountList = new List<long>();
+var traversalStyle = new List<Point>
+{
+    new(1, 1),
+    new(3, 1),
+    new(5, 1),
+    new(7, 1),
+    new(1, 2),
+};
+
+foreach (var traversal in traversalStyle)
+{
+    var traversalTreeCount = 0;
+    var column = traversal.X;
+
+    for (var row = traversal.Y; row < inputRows.Length; row += traversal.Y)
+    {
+        if (inputRows[row][column] == '#')
+        {
+            traversalTreeCount++;
+        }
+
+        column = (column + traversal.X) % rowLength;
+    }
+
+    treeCountList.Add(traversalTreeCount);
+}
+
+Console.WriteLine($"Result part one: {treeCountList[1]}");
+Console.WriteLine($"Result part two: {treeCountList.Aggregate((sum, num) => sum * num)}");
