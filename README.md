6552900879095e0fa6767e8b625dba80faab3dbd
Olav Gjerde <fruithut@tutanota.com>
2020-12-05 14:14:52 +0100

    Adds day 5 part 1 & 2 with alternatives

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Solutions to [Advent Of Code 2020](https://adventofcode.com/2020)
 |[2: Password Philosophy](https://adventofcode.com/2020/day/2)|[1+2](/puzzle2/Program.cs)|C#
 |[3: Toboggan Trajectory](https://adventofcode.com/2020/day/3)|[1+2](/puzzle3/Program.cs)|C#
 |[4: Passport Processing](https://adventofcode.com/2020/day/4)|[1+2](/puzzle4/Program.cs)|C#
+|[5: Binary Boarding](https://adventofcode.com/2020/day/5)|[1+2](/puzzle5/Program.cs)|C#
---
+++ puzzle5/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+var seats = File.ReadAllLines("./input.txt");
+
+SolveUsingDivisionApproach(seats);
+SolveUsingBinaryApproach(seats);
+
+static void SolveUsingDivisionApproach(IEnumerable<string> seats)
+{
+    var seatIds = seats
+        .Select(x => GetRowFromBoardingSpecification(x) * 8 + GetColFromBoardingSpecification(x))
+        .OrderBy(x => x)
+        .ToList();
+
+    var largestId = seatIds.Last();
+    var missingId = 0;
+
+    for (var i = 1; i < seatIds.Count; i++)
+    {
+        var difference = seatIds[i] - seatIds[i - 1];
+        if (difference != 1)
+        {
+            missingId = seatIds[i] - 1;
+            break;
+        }
+    }
+
+    Console.WriteLine($"Result part one: {largestId}");
+    Console.WriteLine($"Result part two: {missingId}");
+}
+
+static void SolveUsingBinaryApproach(IEnumerable<string> seats)
+{
+    var seatIdsAlt = seats.Select(x =>
+    {
+        var binary = GetBinaryRepresentation(x);
+        var row = Convert.ToInt32(binary.Substring(0, 7), 2);
+        var col = Convert.ToInt32(binary.Substring(7, 3), 2);
+        return row * 8 + col;
+    }).OrderBy(x => x).ToList();
+
+    var smallestId = seatIdsAlt.First();
+    var largestId = seatIdsAlt.Last();
+
+    // Assumes 1 missing number
+    var missingId = Enumerable.Range(smallestId, largestId).Except(seatIdsAlt).First();
+
+    Console.WriteLine($"Result part one alternative: {largestId}");
+    Console.WriteLine($"Result part two alternative: {missingId}");
+}
+
+static string GetBinaryRepresentation(string boardSpec)
+{
+    return new StringBuilder(boardSpec)
+        .Replace("F", "0")
+        .Replace("B", "1")
+        .Replace("L", "0")
+        .Replace("R", "1")
+        .ToString();
+}
+
+static int GetColFromBoardingSpecification(string boardSpec)
+{
+    int lower = 0, upper = 7;
+
+    for (var i = 7; i < boardSpec.Length - 1; i++)
+    {
+        var currentCharacter = boardSpec[i];
+        switch (currentCharacter)
+        {
+            case 'L':
+                upper = (lower + upper) / 2;
+                break;
+            case 'R':
+                lower = (lower + upper - 1) / 2 + 1;
+                break;
+        }
+    }
+
+    return boardSpec[9] == 'L' ? lower : upper;
+}
+
+static int GetRowFromBoardingSpecification(string boardSpec)
+{
+    int lower = 0, upper = 127;
+
+    for (var i = 0; i < boardSpec.Length - 4; i++)
+    {
+        var currentCharacter = boardSpec[i];
+
+        switch (currentCharacter)
+        {
+            case 'F':
+                upper = (lower + upper) / 2;
+                break;
+            case 'B':
+                lower = (lower + upper - 1) / 2 + 1;
+                break;
+        }
+    }
+
+    return boardSpec[6] == 'F' ? lower : upper;
+}
