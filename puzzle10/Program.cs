f6d40504070414908a1792861b428ba89d5c44c9
Olav Gjerde <fruithut@tutanota.com>
2020-12-11 00:24:58 +0100

    Adds day 10 part 2
    Also link to good discussion around solution and how it works

--- puzzle10/Program.cs
+++ puzzle10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,22 +10,49 @@ namespace puzzle10
         static void Main(string[] args)
         {
             var adapters = File.ReadLines("./input.txt").Select(int.Parse).ToList();
-            adapters.Insert(0, 0);
-            adapters.Add(adapters.Last() + 3);
             adapters.Sort();
+            adapters.Add(adapters.Last() + 3);
 
+            var resultPartOne = FindProductOfOneAndThreeDifference(adapters);
+            var resultPartTwo = FindAllCombinations(adapters);
+
+            Console.WriteLine($"Result part one: {resultPartOne}");
+            Console.WriteLine($"Result part two: {resultPartTwo}");
+        }
+        
+        private static int FindProductOfOneAndThreeDifference(List<int> sortedAdapters)
+        {
             int currentJolt = 0, oneOffCount = 0, threeOffCount = 0;
-            foreach (var joltRating in adapters.ToHashSet())
+            foreach (var joltRating in sortedAdapters)
             {
-                var diff = joltRating - currentJolt;
-
-                if (diff == 1) oneOffCount++;
-                else if (diff == 3) threeOffCount++;
+                var difference = joltRating - currentJolt;
+                if (difference == 1) oneOffCount++;
+                else if (difference == 3) threeOffCount++;
 
                 currentJolt = joltRating;
             }
 
-            Console.WriteLine($"Result part one: {oneOffCount * threeOffCount}");
+            return oneOffCount * threeOffCount;
+        }
+
+        /// <summary>
+        /// Found part 2 difficult, see discussion and inspiration below here:
+        /// <see href="https://www.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/gf9pg9n/"/>
+        /// </summary>
+        /// <param name="sortedAdapters">Numbers in ascending order</param>
+        /// <returns>All the ways one can access the last adapter</returns>
+        private static long FindAllCombinations(List<int> sortedAdapters)
+        {
+            var ways = new Dictionary<long, long> {{0, 1}};
+            foreach (var currentAdapter in sortedAdapters)
+            {
+                ways.TryGetValue(currentAdapter - 1, out var accessViaNegative1);
+                ways.TryGetValue(currentAdapter - 2, out var accessViaNegative2);
+                ways.TryGetValue(currentAdapter - 3, out var accessViaNegative3);
+                ways[currentAdapter] = accessViaNegative1 + accessViaNegative2 + accessViaNegative3;
+            }
+
+            return ways[sortedAdapters.Last()];
         }
     }
 }
