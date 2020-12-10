e6e50a738edbe1b4dce921b968683ccbd1e6ca31
Olav Gjerde <fruithut@tutanota.com>
2020-12-10 23:27:26 +0100

    Adds day 10 part 1

---
+++ puzzle10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace puzzle10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adapters = File.ReadLines("./input.txt").Select(int.Parse).ToList();
+            adapters.Insert(0, 0);
+            adapters.Add(adapters.Last() + 3);
+            adapters.Sort();
+
+            int currentJolt = 0, oneOffCount = 0, threeOffCount = 0;
+            foreach (var joltRating in adapters.ToHashSet())
+            {
+                var diff = joltRating - currentJolt;
+
+                if (diff == 1) oneOffCount++;
+                else if (diff == 3) threeOffCount++;
+
+                currentJolt = joltRating;
+            }
+
+            Console.WriteLine($"Result part one: {oneOffCount * threeOffCount}");
+        }
+    }
+}
